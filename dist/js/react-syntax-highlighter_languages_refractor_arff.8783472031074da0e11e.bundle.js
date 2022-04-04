ca2fbf56f8b34f66ce4ec135d46e364cf29e5cd4
Mickel <mickelelkhoury-work@outlook.com>
2022-04-04 09:19:53 +0300

    added build folder

---
+++ dist/js/react-syntax-highlighter_languages_refractor_arff.8783472031074da0e11e.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[1438],{60274:e=>{function n(e){e.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}e.exports=n,n.displayName="arff",n.aliases=[]}}]);
---
+++ dist/js/react-syntax-highlighter_languages_refractor_ini.8c8dcd9fa523d386dd5c.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[6495],{14970:t=>{function n(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=n,n.displayName="ini",n.aliases=[]}}]);
